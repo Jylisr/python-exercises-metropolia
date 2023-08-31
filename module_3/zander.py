86aca1f8b8ecbfb9201fd7e2cb018cb250e1c55a
WalmartWarchief <juho.ylisir@gmail.com>
2023-08-31 03:46:29 +0300

    Added module 3 task 1

---
+++ module_3/zander.py
@@ -0,0 +1,7 @@
+#Module 3, task 1
+zander = (float(input("Enter the lenght of the zander in centimeters: ")))
+
+if zander >= 42:
+    print("The zander fulfills the size limit")
+else:
+    print("The zander is too small, it has to be 42 centimeters or longer")
