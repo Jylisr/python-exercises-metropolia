2491f77c1f2bc32a0aab5a05934956eb389a67d7
WalmartWarchief <juho.ylisir@gmail.com>
2023-09-08 00:02:23 +0300

    Module 4

--- module_4/m4_t5.py
+++ module_4/m4_t5.py
@@ -1,1 +1,18 @@
 #Module 4, task 5
+correct_username = "python"
+correct_password = "rules"
+
+attempts = 0
+
+while attempts < 5:
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    if username == correct_username and password == correct_password:
+        print("Welcome")
+        break
+    else:
+        print("Incorrect username or password. Please try again.")
+        attempts += 1
+if attempts == 5:
+    print("Access denied")
