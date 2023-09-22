7679e08fc83e8e9bd446708707eb78df672499ad
Juho <juho.ylisir@gmail.com>
2023-09-22 14:28:25 +0300

    Module 7

--- module_7/m7_t2.py
+++ module_7/m7_t2.py
@@ -5,7 +5,7 @@ set_names = set("J")
 while True:
     nam_es = str(input("Enter names: "))
     if nam_es == "":
-        print(set_names)
+        print(set_names, sep = "\n")
         break
     if nam_es in set_names:
         print("Existing name")
--- module_7/m7_t3.py
+++ module_7/m7_t3.py
@@ -1,3 +1,14 @@
 #Module 7, task 3
+airports = {"EFHK" : "Helsinki-Vantaa airport",}
+choice = input("Would you like to add a new airport or search and existing one? Enter add or search: ")
 
+if choice == "add":
+    while True:
+        break
+
+if choice == "search":
+    while True:
+        icao = input("Enter icao")
+        if icao in airports:
+            print(airports[])
 
