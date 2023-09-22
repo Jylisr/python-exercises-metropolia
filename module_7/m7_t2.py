300d7760f929729bb4ddeff6e1663050fb4b870c
Juho <juho.ylisir@gmail.com>
2023-09-22 14:02:28 +0300

    Module 7

--- module_7/m7_t2.py
+++ module_7/m7_t2.py
@@ -1,11 +1,15 @@
 #Module 7, task 2
 
-set_names = set()
+set_names = set("J")
 
 while True:
     nam_es = str(input("Enter names: "))
     if nam_es == "":
+        print(set_names)
         break
+    if nam_es in set_names:
+        print("Existing name")
+    else:
+        print("New name")
     set_names.add(nam_es)
 
-print(set_names)
