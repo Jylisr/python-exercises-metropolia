805eaee0eed1e6af898f89d6c4d637cd805e0ab9
WalmartWarchief <juho.ylisir@gmail.com>
2023-09-26 00:18:09 +0300

    Module 7

--- module_7/m7_t3.py
+++ module_7/m7_t3.py
@@ -1,14 +1,26 @@
 #Module 7, task 3
 airports = {"EFHK" : "Helsinki-Vantaa airport",}
-choice = input("Would you like to add a new airport or search and existing one? Enter add or search: ")
+choice = input("Would you like to add a new airport or search and existing one? Enter add or search, enter quit to exit: ")
 
-if choice == "add":
-    while True:
+while True:
+    if choice == "add":
+        while True:
+            icao = input("Enter icao: ")
+            name = input("Enter airport name: ")
+            if icao or name == "":
+                break
+            else:
+                airports.update({icao : name})
+                print(airports)
+
+    elif choice == "search":
+        while True:
+            icao = input("Enter icao: ")
+            if icao == "":
+                print(airports)
+                break
+            elif icao in airports:
+                print(airports[icao])
+    elif choice == "quit" or "":
         break
-
-if choice == "search":
-    while True:
-        icao = input("Enter icao")
-        if icao in airports:
-            print(airports[])
 
