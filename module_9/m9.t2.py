f7dbf6e836ae5152e16c178c0488bf713e5180b5
WalmartWarchief <juho.ylisir@gmail.com>
2023-10-25 02:17:31 +0300

    Module 9

--- module_9/m9.t2.py
+++ module_9/m9.t2.py
@@ -1,1 +1,29 @@
 #Module 9, task 2
+
+class Car:
+    def __init__(self, reg_num, max_spd):
+        self.reg_num = reg_num
+        self.max_spd = max_spd
+        self.cur_spd = 0
+        self.tra_dis = 0
+
+    def accelerate(self):
+        if self.cur_spd == 0 and self.cur_spd <= self.max_spd:
+            self.cur_spd + 30
+            self.cur_spd + 70
+            self.cur_spd + 50
+            print(self.cur_spd)
+
+    def brake(self):
+        self.cur_spd - 200
+        
+        
+    
+car1 = Car("ABC-123", 142)
+
+print(car1.reg_num)
+print(car1.max_spd)
+print(car1.cur_spd)
+print(car1.tra_dis)
+
+car1.accelerate()
--- module_9/m9_t1.py
+++ module_9/m9_t1.py
@@ -1,7 +1,16 @@
 #Module 9, task 1
 class Car:
-    def __init__(self, reg_num, max_spd, cur_spd, tra_dis)
+    def __init__(self, reg_num, max_spd):
+        self.reg_num = reg_num
+        self.max_spd = max_spd
+        self.cur_spd = 0
+        self.tra_dis = 0
+        
     
     
-    
-car1 = Car()
+car1 = Car("ABC-123", "142 kmh/h")
+
+print(car1.reg_num)
+print(car1.max_spd)
+print(car1.cur_spd)
+print(car1.tra_dis)
