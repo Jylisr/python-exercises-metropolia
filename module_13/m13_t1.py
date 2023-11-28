524bfc2833315807a68f3b4687d378fa473f113f
Juho <juho.ylisir@gmail.com>
2023-11-28 19:42:10 +0200

    28.11.2023

--- module_13/m13_t1.py
+++ module_13/m13_t1.py
@@ -2,26 +2,26 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/prime_number')
-def prime_number():
-    args = request.args
-    num = int(args.get("num"))
-    prime_num = False
-
-    if num <= 1:
-        result = {"num": num, "is_prime": False, "message": "Not a prime number"}
-    else:
-        for i in range(2, num):
-            if (num % i) == 0:
-                prime_num = True
+@app.route('/prime_number/<num>')
+def prime_number(num):
+    num = int(num)
+    if num > 1:
+        for i in range(2, int(num / 2) + 1):
+            if num % i == 0:
+                prime = f"{num} is not a prime number"
                 break
+            else:
+                prime = f"{num} is a prime number"
+    else:
+        prime = f"{num} is a prime number"
+    response = {
+        "prime" : prime,
+        "status" : 200
+    }
+    return response
 
-        if not prime_num:
-            result = {"num": num, "is_prime": True, "message": f"{num} is a prime number"}
-        else:
-            result = {"num": num, "is_prime": False, "message": f"{num} is not a prime number"}
 
-    return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=5000)
